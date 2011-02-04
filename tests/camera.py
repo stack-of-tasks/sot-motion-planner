281453efb11b0e89deb1d8af2b73fce9db071bf7
Thomas Moulard <thomas.moulard@gmail.com>
2011-02-04 14:24:32 +0100

    Enhance camera.py test case.

--- tests/camera.py
+++ tests/camera.py
@@ -16,7 +16,7 @@
 # sot-motion-planner. If not, see <http://www.gnu.org/licenses/>.
 
 import numpy as np
-from math import acos, cos, sin, pi
+from math import acos, atan2, cos, sin, pi, sqrt
 
 from dynamic_graph import plug
 from dynamic_graph.sot.motion_planner import Localizer
@@ -51,7 +51,7 @@ def makeHomogeneousMatrix(R = None, t = None):
 def getR(H):
     return H[0:3,0:3]
 def getT(H):
-    return H[0:3,3:].transpose()[0]
+    return np.array([H[0,3], H[1,3], H[2,3]], dtype=np.float)
 
 # rotation vector representation
 def makeRotationVector(x = 0., y = 0., z = 0.):
@@ -78,6 +78,17 @@ def rotationMatrixToRotationVector(rotationMatrix):
     v[2] = rotationMatrix[1,0] - rotationMatrix[0,1]
     v *= theta / (2. * sin(theta))
     return v
+
+# roll, pitch, yaw
+def roll(rotationMatrix):
+    return atan2(rotationMatrix[1,0], rotationMatrix[0,0])
+
+def pitch(rotationMatrix):
+    return atan2(-rotationMatrix[2,0],
+                  sqrt(rotationMatrix[2,1]**2 + rotationMatrix[2,2]**2))
+
+def yaw(rotationMatrix):
+    return atan2(rotationMatrix[2,1], rotationMatrix[2,2])
 
 
 ###################################
@@ -125,8 +136,58 @@ if checkAlgebra:
 #
 # gaze operational point gives the camera optical frame
 #
+# q = (x, y, theta)
+#
+# S(q) = (x, y, z?, u, v, w)
+# P(S(q)) = (x, y)
+#
+#            dS/dx, dS/dy, dS/dtheta
+# dS(q) = [[ 1      0                ] x
+#          [ 0      1                ] y
+#          [ 0      0      0         ] z
+#          [ 0      0                ] u
+#          [ 0      0                ] v
+#          [ 0      0                ] w
 #
 #
+#
+# APPROACH 1: consider (u, v, w) = (0, 0, theta)
+#
+#            dS/dx, dS/dy, dS/dtheta
+# dS(q) = [[ 1      0      -x * sin(theta) - y * cos(theta) ] x
+#          [ 0      1      x * cos(theta) - y * sin(theta)  ] y
+#          [ 0      0      0                                ] z
+#          [ 0      0      0                                ] u
+#          [ 0      0      0                                ] v
+#          [ 0      0      1                                ] w
+#
+# x' = x * cos(theta) - y * sin(theta)
+# y' = x * sin(theta) + y * cos(theta)
+#
+# dtheta = [[ -x * sin(theta) - y * cos(theta) ]
+#           [  x * cos(theta) - y * sin(theta) ]]
+#
+# ---
+#
+#               dS/dx dS/dy dS/dz dS/du dS/dv dS/dw
+# dP(s(q)) = [[                                     ] x
+#             [                                     ] y
+#
+#
+# P = C * [[1,0,0,0] * e^s(q) * [[x_ref]
+#          [0,1,0,0]             [y_ref]
+#          [0,0,1,0]]            [z_ref]]
+#
+#
+# P = C * [[1,0,0,0]
+#          [0,1,0,0]
+#          [0,0,1,0]]
+#     * (I
+#        + s(q)^ / |s(q)| * sin(|s(q)|)
+#        + (s(q)^)^2 / |s(q)|^2 * (1 - cos(|s(q)|)))
+#     * [[x_ref]
+#        [y_ref]]
+#        [z_ref]]
 #
 #
 #
@@ -155,7 +216,7 @@ C = np.matrix(
     )
 
 def S(q, sensorId):
-    return robot.dynamic.gaze.value
+    return np.matrix(robot.dynamic.gaze.value, dtype=np.float)
 
 def P(sensorPosition, landmark):
     _3to4 = np.matrix(
@@ -171,7 +232,24 @@ def P(sensorPosition, landmark):
     return np.array([tmp[0]/tmp[2], tmp[1]/tmp[2]], dtype=np.float)
 
 def dS(q, sensorId):
-    return robot.dynamic.Jgaze.value #FIXME:
+    value = np.matrix(robot.dynamic.gaze.value)
+    J = np.matrix(robot.dynamic.Jgaze.value, dtype=np.float)
+    m = np.zeros(shape=(6,3), dtype=np.float)
+    m[0:5,0:2] = J[0:5,0:2]
+
+    m[2,0:2] = [0.,0.] #dS/dZ is null
+
+    (x, y, theta) = (getT(value)[0],
+                     getT(value)[1],
+                     yaw(getR(value)))
+
+    m[0,2] = -x * sin(theta) - y * cos(theta)
+    m[1,2] = x * cos(theta) - y * sin(theta)
+    m[2,2] = 0.
+    m[3,2] = 0.
+    m[4,2] = 0.
+    m[5,2] = 1.
+    return m
 
 def dP(sensorPosition, landmark):
     pass #FIXME:
@@ -179,4 +257,19 @@ def dP(sensorPosition, landmark):
 
 ###############################
 
-print P(np.matrix(robot.dynamic.gaze.value), 0)
+print "GAZE"
+print S(0,0)
+print "JGAZE (position + rotation vector)^2"
+print np.matrix(robot.dynamic.Jgaze.value)[0:6,0:6]
+
+print "P"
+print P(S(0,0), 0)
+
+print "dS"
+print dS(0,0)
+
+print "dP"
+#print dP(S(0, 0),0)
+
+# TODO: implementer dP
+# verifier projection vecteur rotation sur axe Z
