efd41672251d3e0a76373c41136edfd4318d535d
Thomas Moulard <thomas.moulard@gmail.com>
2011-04-19 12:40:42 +0200

    Make script more robust.

--- src/dynamic_graph/sot/motion_planner/test/feet_follower_analytical_pg_correction.py
+++ src/dynamic_graph/sot/motion_planner/test/feet_follower_analytical_pg_correction.py
@@ -15,6 +15,7 @@
 # received a copy of the GNU Lesser General Public License along with
 # sot-motion-planner. If not, see <http://www.gnu.org/licenses/>.
 
+from dynamic_graph.sot.core import RobotSimu
 from dynamic_graph.sot.dynamics.tools import *
 from dynamic_graph.sot.motion_planner.feet_follower_graph import *
 from __main__ import robot, solver
@@ -27,13 +28,14 @@ from dynamic_graph.sot.motion_planner.feet_follower_graph \
 
 from dynamic_graph.corba_server import CorbaServer
 
-enableMocap=False
+onRobot=type(robot.device) != RobotSimu
+enableMocap=True
+
+print("Is this the robot? " + str(onRobot))
+print("Are we using mocap? "+ str(enableMocap))
 
 # Launch corba server.
 corba = CorbaServer('corba')
-
-if enableMocap:
-    raw_input("Start evart-to-client, please.")
 
 # first slide # hor distance # max feet height # second slide # x # y # theta
 steps = [
@@ -50,8 +52,6 @@ steps = [
     ]
 
 
-
-
 f = FeetFollowerAnalyticalPgGraph(steps)
 f.referenceTrajectory = f.feetFollower
 f.feetFollower = FeetFollowerWithCorrection('correction')
@@ -63,10 +63,14 @@ plug(robot.dynamic.waist, f.feetFollower.waist)
 
 # Set the safety limits.
 # Max rotation alone: 3.14/8.
-(maxX, maxY, maxTheta) = (0.1, 0.1, 3.14/10.)
+#(maxX, maxY, maxTheta) = (0.06, 0.03, 0.05)
+
+(maxX, maxY, maxTheta) = (0.04, 0.04, 0.05)
+
+(maxX, maxY, maxTheta) = (0., 0., 0.)
 
 f.feetFollower.setSafetyLimits(maxX, maxY, maxTheta)
-print "Safe limits: %f %f %f" % (maxX, maxY, maxTheta)
+print ("Safe limits: %f %f %f" % (maxX, maxY, maxTheta))
 
 # Make up some error value.
 f.randomizer = Randomizer('r')
@@ -78,17 +82,6 @@ f.errorEstimator.setReferenceTrajectory(f.referenceTrajectory.name)
 plug(robot.dynamic.waist, f.errorEstimator.waist)
 #plug(f.errorEstimator.error, f.feetFollower.offset)
 
-# Motion capture
-if enableMocap:
-    #FIXME: here we are supposing that the dg world frame matches
-    # the tile frame.
-    f.errorEstimator.setWorldTransformation(corba.tilePosition.value)
-    plug(corba.waistPosition, f.errorEstimator.position)
-    plug(corba.waistPositionTimestamp, f.errorEstimator.positionTimestamp)
-else:
-    f.errorEstimator.position.value = (0., 0., 0.)
-    f.errorEstimator.positionTimestamp.value = (0., 0.)
-
 # Replug.
 plug(f.feetFollower.zmp, robot.device.zmp)
 plug(f.feetFollower.com, robot.featureComDes.errorIN)
@@ -96,6 +89,47 @@ plug(f.feetFollower.signal('left-ankle'),
      robot.features['left-ankle'].reference)
 plug(f.feetFollower.signal('right-ankle'),
      robot.features['right-ankle'].reference)
+
+def plugMocap():
+    if len(corba.signals()) == 3:
+        print ("evart-to-client not launched, abandon.")
+        return
+    #raw_input("Start evart-to-client, please.")
+
+    #FIXME: here we are supposing that the dg world frame matches
+    # the tile frame.
+    #f.errorEstimator.setWorldTransformation(corba.tilePosition.value)
+
+    (x, y, z) = (
+        corba.waistPosition.value[0],
+        corba.waistPosition.value[1],
+        robot.dynamic.waist.value[3][3]
+        )
+    M = ((1., 0., 0., x),
+         (0., 1., 0., y),
+         (0., 0., 1., z),
+         (0., 0., 0., 1.))
+    print (M)
+
+    f.errorEstimator.setWorldTransformation(M)
+
+    plug(corba.waistPosition, f.errorEstimator.position)
+    plug(corba.waistPositionTimestamp, f.errorEstimator.positionTimestamp)
+
+# Motion capture
+if enableMocap:
+    if not onRobot:
+        while len(corba.signals()) == 3:
+            raw_input("corba")
+        plugMocap()
+    else:
+        print ("Type 'plugMocap()'")
+        print ("then 'f.start(logRef)'")
+else:
+    f.errorEstimator.position.value = (0., 0., 0.)
+    f.errorEstimator.positionTimestamp.value = (0., 0.)
+    print ("disable error estimator")
+
 
 # Trace
 def logRef():
@@ -115,3 +149,4 @@ def logRef():
 
     f.trace.add(f.errorEstimator.name + '.' + 'error',
                 f.errorEstimator.name + '-' + 'error')
+    print ("logging reference trajectory")
