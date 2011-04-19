eeec8d9ec6c71ef5724a40f49655cf54a49903a9
Thomas Moulard <thomas.moulard@gmail.com>
2011-04-19 15:15:53 +0200

    Add error estimator/mocap to feet_follower_analytical_pg_step2.py.

--- src/dynamic_graph/sot/motion_planner/test/feet_follower_analytical_pg_step2.py
+++ src/dynamic_graph/sot/motion_planner/test/feet_follower_analytical_pg_step2.py
@@ -15,11 +15,20 @@
 # received a copy of the GNU Lesser General Public License along with
 # dynamic-graph. If not, see <http://www.gnu.org/licenses/>.
 
+from dynamic_graph.sot.core import RobotSimu
 from dynamic_graph.sot.dynamics.tools import *
 from dynamic_graph.sot.motion_planner.feet_follower_graph import *
+from dynamic_graph.sot.motion_planner \
+    import Localizer, FeetFollowerWithCorrection, Randomizer, ErrorEstimator
 from __main__ import robot, solver
 
 from dynamic_graph.corba_server import CorbaServer
+
+onRobot=type(robot.device) != RobotSimu
+enableMocap=True
+
+print("Is this the robot? " + str(onRobot))
+print("Are we using mocap? "+ str(enableMocap))
 
 corba = CorbaServer('corba')
 
@@ -53,12 +62,60 @@ steps = [
 
 f = FeetFollowerAnalyticalPgGraph(steps)
 
-def logWaistPos():
+f.errorEstimator = ErrorEstimator('error_estimator')
+f.errorEstimator.setReferenceTrajectory(f.feetFollower.name)
+plug(robot.dynamic.waist, f.errorEstimator.waist)
+
+robot.device.after.addSignal(robot.dynamic.name + '.' + 'waist')
+
+def plugMocap():
+    if len(corba.signals()) == 3:
+        print ("evart-to-client not launched, abandon.")
+        return
+    #raw_input("Start evart-to-client, please.")
+
+    #FIXME: here we are supposing that the dg world frame matches
+    # the tile frame.
+    #f.errorEstimator.setWorldTransformation(corba.tilePosition.value)
+
+    (x, y, z) = (
+        corba.waistPosition.value[0],
+        corba.waistPosition.value[1],
+        robot.dynamic.waist.value[3][3]
+        )
+    M = ((1., 0., 0., x),
+         (0., 1., 0., y),
+         (0., 0., 1., z),
+         (0., 0., 0., 1.))
+    print (M)
+
+    f.errorEstimator.setWorldTransformation(M)
+
+    plug(corba.waistPosition, f.errorEstimator.position)
+    plug(corba.waistPositionTimestamp, f.errorEstimator.positionTimestamp)
+
+# Motion capture
+if enableMocap:
+    if not onRobot:
+        while len(corba.signals()) == 3:
+            raw_input("Press enter after starting evart-to-corba.")
+        plugMocap()
+    else:
+        print ("Type 'plugMocap()'")
+        print ("then 'f.start(logRef)'")
+else:
+    f.errorEstimator.position.value = (0., 0., 0.)
+    f.errorEstimator.positionTimestamp.value = (0., 0.)
+    print ("disable error estimator")
+
+
+def logRef():
     f.trace.add(corba.name + '.waistPosition',
                 corba.name + '-waistPosition')
     robot.device.after.addSignal(corba.name + '.waistPosition')
 
-def logWaistPosInteractive():
-    print "Press enter after starting evart-to-corba."
-    raw_input()
-    logWaistPos()
+    f.trace.add(f.errorEstimator.name + '.' + 'error',
+                f.errorEstimator.name + '-' + 'error')
+    robot.device.after.addSignal(f.errorEstimator.name + '.' + 'error')
+
+logWaistPosInteractive = logRef
